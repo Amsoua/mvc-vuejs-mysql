7a72811d5383676fdc71bbf6032746b8d3c7af91
Amadou SOUANE <souane.ame@gmail.com>
2019-05-20 15:17:29 +0000

    first commit

---
+++ src/main/java/com/springboot/vuejs/fullstack/springbootmvcvuejsmysql/SpringbootmvcVuejsMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.vuejs.fullstack.springbootmvcvuejsmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcVuejsMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmvcVuejsMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/vuejs/fullstack/springbootmvcvuejsmysql/SpringbootmvcVuejsMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.vuejs.fullstack.springbootmvcvuejsmysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootmvcVuejsMysqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
